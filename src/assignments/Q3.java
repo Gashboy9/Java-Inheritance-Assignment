5d99364466be7cd5636b1b4b12cb983a4298224b
gashboy9 <gashboy@yahoo.com>
2021-03-11 22:42:15 +0000

    Question 3 completed

---
+++ src/assignments/Q3.java
@@ -0,0 +1,52 @@
+package assignments;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle rect1 = new Rectangle(2,3);
+		System.out.println("Area of a rectangle is: "+rect1.area());
+		System.out.println("Perimeter of a rectangle is: "+rect1.perimeter());
+		
+		Square sq1 = new Square (7);
+		System.out.println("Area of a square is: "+sq1.area());
+		System.out.println("Perimeter of a square is: "+sq1.perimeter());
+
+
+	}
+
+}
+
+
+
+class Rectangle{
+	int length, breadth;
+	
+	public Rectangle(int length, int breadth) {
+		this.length = length;
+		this.breadth = breadth;
+	}
+	
+	public double area() {
+		double result = length * breadth;
+		//System.out.println("Area of rectangle is: "+result);
+		return result;
+	}
+	
+	public double perimeter() {
+		double result = 2*(length + breadth);
+		//System.out.println("Area of rectangle is: "+result);
+		return result;
+	}
+
+}
+
+
+class Square extends Rectangle{
+
+	public Square(int s) {
+		super(s, s);
+	}
+	
+}
